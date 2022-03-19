5b4a2de796f078e9f13b03da26c5a869204f8cb2
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-03-19 17:06:28 +0530

    Added solution - LeetHub

--- Check if strings are rotations of each other or not - GFG/check-if-strings-are-rotations-of-each-other-or-not.cpp
+++ Check if strings are rotations of each other or not - GFG/check-if-strings-are-rotations-of-each-other-or-not.cpp
@@ -16,21 +16,25 @@ class Solution
         int m = s2.length();
         if (n != m)
         return false;
-        // vector<int> a1(26,0),a2(26,0);
-        // for (int i = 0; i < n; i++)
-        // {
-        //     a1[s1[i] - 97]++;
-        //     a2[s2[i] - 97]++;
-        // }
-        // for (int i = 0; i < 26; i++)
-        // {
-        //     if (a1[i] != a2[i])
-        //     return false;
-        // }
-        string s3 = s1 + s1;
+        vector<int> a1(26,0),a2(26,0);
+        queue<char> q1,q2;
         for (int i = 0; i < n; i++)
         {
-            if (s3.substr(i,n) == s2)
+            a1[s1[i] - 97]++;
+            a2[s2[i] - 97]++;
+            q1.push(s1[i]);
+            q2.push(s2[i]);
+        }
+        for (int i = 0; i < 26; i++)
+        {
+            if (a1[i] != a2[i])
+            return false;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            q1.pop();
+            q1.push(s1[i]);
+            if (q1 == q2)
             return true;
         }
         return false;
