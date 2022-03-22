cd1e18507e24c9fc14d388d838516097052bbc02
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-03-22 20:21:45 +0530

    Time: 2 ms (43.22%), Space: 5.9 MB (45.22%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,6 +1,35 @@
 class Solution {
 public:
+    double power(double x,int n)
+    {
+        if (n == 0)
+            return 1;
+        if (n%2 == 1 || n%2 == -1)
+        {
+            return x*power(x,n-1);
+        }
+        double y = power(x,n/2);
+        return y*y;
+    }
     double myPow(double x, int n) {
-        return pow(x,n);
+        if (x == 1.00000)
+            return x;
+        else if (x == -1.00000)
+        {
+            if (abs(n)%2 == 0)
+                return -x;
+            else
+                return x;
+        }
+        if (n>=0)
+        {
+            return power(x,n);
+        }
+        else
+        {
+            if (n == -2147483648)
+                return 0;
+            return 1/power(x,-n);
+        }
     }
 };
