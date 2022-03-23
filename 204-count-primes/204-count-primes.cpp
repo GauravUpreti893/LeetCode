d6342e5696363f4232d87504dfefb9533652aa1f
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-03-23 07:14:53 +0530

    Time: 735 ms (7.44%), Space: 130.6 MB (7.55%) - LeetHub

--- 204-count-primes/204-count-primes.cpp
+++ 204-count-primes/204-count-primes.cpp
@@ -3,11 +3,12 @@ public:
     int countPrimes(int n) {
         if (n <= 2)
             return 0;
-        int *arr = new int[n];
-        for (int i = 2; i < n; i++)
-        {
-            arr[i] = 1;
-        }
+        // int *arr = new int[n];
+        // for (int i = 2; i < n; i++)
+        // {
+        //     arr[i] = 1;
+        // }
+        vector<int> arr(n,1);
         arr[0] = 0;
         arr[1] = 0;
         int ans = 0;
