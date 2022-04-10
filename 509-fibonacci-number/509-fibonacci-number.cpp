1eded5041c932f60330d0f8cc088b44200d27d49
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-04-10 12:15:27 +0530

    Time: 3 ms (54.29%), Space: 6.1 MB (18.24%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,8 +1,20 @@
 class Solution {
 public:
+    int fibo(int n, int* &table)
+    {
+        if (table[n] == -1)
+        {
+            table[n] = fibo(n - 1,table) + fibo(n - 2, table);
+        }
+        return table[n];
+    }
     int fib(int n) {
-        if (n <= 1)
-            return n;
-        return fib(n - 1) + fib(n - 2);
+        int *table = new int[n + 1];
+        for (int i = 0; i <= n; i++)
+            table[i] = -1;
+        table[0] = 0;
+        if (n != 0)
+        table[1] = 1;
+        return fibo(n,table);
     }
 };
