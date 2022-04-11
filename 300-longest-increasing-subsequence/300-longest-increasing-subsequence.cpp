5fe8020bd23ce56a6152888c16696b6bbf14130c
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-04-11 12:07:04 +0530

    Time: 653 ms (6.65%), Space: 10.9 MB (8.94%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -6,18 +6,13 @@ public:
         {
             for (int i = n - 1; i > 0; i--)
             {
+                if (table[i] == -1)
+                    table[i] = len(nums,i,table);
                 if (nums[i - 1] < nums[n - 1])
                 {
                     table[n] = max(table[n],len(nums,i,table) + 1);
                 }
-                else if (nums[i - 1] == nums[n - 1])
-                {
-                    table[n] = max(table[n],len(nums,i,table));
-                }
-                else
-                {
-                    table[n] = max(table[n],1);
-                }
+                
                 
             }
             table[n] = max(table[n],1);
@@ -32,11 +27,8 @@ public:
             table[i] = -1;
         }
         table[0] = 0;
-        for (int i = 1; i<= n; i++)
-        {
-            if (table[i] == -1)
-            len(nums,i,table);
-        }
+    
+        len(nums,n,table);
         int ans = 0;
         for (int i = 1; i <= n; i++)
         {
