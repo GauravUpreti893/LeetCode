0e03b059b1d3fdc5e8dd50307351af4558d1c199
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-04-16 19:40:06 +0530

    Time: 39 ms (30.64%), Space: 13 MB (50.13%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,31 +1,60 @@
 class Solution {
 public:
-    int longestCommonSubsequence(string text1, string text2) {
+    int **table;
+    int longest(string &text1, string&text2, int n, int m)
+    {
+        if (table[n][m] >= 0)
+            return table[n][m];
+            if (text1[n - 1] == text2[m - 1])
+            {
+                table[n][m] = longest(text1,text2,n - 1, m - 1) + 1;
+            }
+            else
+                table[n][m] = max(longest(text1,text2, n - 1, m ), longest(text1,text2, n, m - 1));
+        return table[n][m];
+    }
+    int longestCommonSubsequence(string &text1, string& text2) {
         int n = text1.size();
         int m = text2.size();
-        vector<vector<int>> table(n + 1,vector<int> (m + 1));
-        for (int i = 0; i <= m; i++)
-        {
-            table[0][m] = 0;
-        }
+        // vector<vector<int>> table(n + 1,vector<int> (m + 1, -1));
+        table = new int*[n + 1];
         for (int i = 0; i <= n; i++)
         {
-            table[n][0] = 0;
+            table[i] = new int[ m + 1];
         }
-        for (int i = 1; i <= n;i++)
+        // for (int i = 1; i <= n; i++)
+        // {
+        //     for (int j = 1; j <= m; j++)
+        //     {
+        //         table[i][j] = -1;
+        //     }
+        // }
+        for (int i = 0; i <= n; i++)
         {
-            for (int j = 1; j <= m; j++)
-            {
-                if (text1[i - 1] == text2[j - 1])
-                {
-                    table[i][j] = table[i - 1][j - 1] + 1;
-                }
-                else
-                {
-                    table[i][j] = max(table[i - 1][j],table[i][j - 1]);
-                }
-            }
+            table[i][0] = 0;
         }
-        return table[n][m];
+        for (int i = 0; i <= m; i++)
+        {
+            table[0][i] = 0;
+        }
+        // for (int i = 0; i <= n; i++)
+        // {
+        //     for (int j = 0; j <= m; j++)
+        //     {
+        //         cout<<table[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        int ans =  longest(text1,text2,n,m);
+        // for (int i = 0; i <= n; i++)
+        // {
+        //     for (int j = 0; j <= m; j++)
+        //     {
+        //         cout<<table[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        return ans;
+        // return 1;
     }
 };
