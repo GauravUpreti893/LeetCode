48b16d5c22b893d6d60204021edf42489044ff89
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-05-12 15:03:48 +0530

    Time: 8 ms (70.09%), Space: 13.9 MB (27.27%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -1,109 +1,18 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int ans = -11;
-        int prod;
+        int ans = nums[0],mx = nums[0], mn = nums[0];
         int n = nums.size();
-        int total = 1;
-        int count = 0;
-        int c = 0, p = 0;
-        int **arr;
-        if (n == 1)
-            return nums[0];
-        for (int i = 0; i < n; i++)
+        for (int i = 1; i < n; i++)
         {
-            if ( i!= 0 && i != n - 1 && nums[i] == 0)
-                count++;
+            if (nums[i] < 0)
+            {
+                swap(mx,mn);
+            }
+           mx = max(nums[i],mx*nums[i]);
+           mn = min(nums[i], mn*nums[i]);
+            ans = max(ans, mx);
         }
-        int *size = new int[count + 1];
-        int mx = -11;
-        for (int i = 0; i < n; i++)
-        {
-            mx = max(mx, nums[i]);
-            if (i == 0 && nums[i] == 0)
-            {
-                
-            }
-            else if (nums[i] == 0)
-            {
-                size[p] = c;
-                p++;
-                c = 0;
-            }
-            else
-            c++;
-        }
-        if (nums[n - 1] != 0)
-        size[p] = c;
-        // else
-        //     count--;
-        // if (nums[0] == 0)
-        //     count--;
-        arr = new int*[count + 1];
-        cout<<count<<endl;
-        cout<<size[0]<<endl;
-       // return 0;
-         for (int i = 0; i <= count; i++)
-        {
-            arr[i] = new int[size[i]];
-        }
-        c = 0;
-        p = 0;
-        // return 0;
-        for (int i = 0; i < n; i++)
-        {
-            if (i == 0 && nums[i] == 0)
-            {
-                
-            }
-            else if (nums[i] == 0)
-            {
-                p++;
-                c = 0;
-                ans = 0;
-            }
-            else
-            {
-                arr[p][c] = nums[i];
-                c++;
-            }
-           
-            
-        }
-       // return 0;
-        for (int j = 0; j <= count; j++)
-        {
-            if (size[j] == 1)
-            {
-                ans = max(ans, arr[j][0]);
-                continue;
-            }
-            prod = 1;
-            int temp;
-            for (int i = 0; i < size[j]; i++)
-            {
-                prod *= arr[j][i];
-            }
-            temp = prod;
-            // cout<<prod<<endl;
-            ans = max(ans, prod);
-            int k = size[j] - 1;
-            while (prod != 1)
-            {
-                prod /= arr[j][k];
-                k--;
-                ans = max(ans, prod);
-            }
-            k = 0;
-            prod = temp;
-             while (prod != 1)
-            {
-                prod /= arr[j][k];
-                k++;
-                ans = max(ans, prod);
-            }
-        }
-        
-        return max(mx,ans);
+        return ans;
     }
 };
