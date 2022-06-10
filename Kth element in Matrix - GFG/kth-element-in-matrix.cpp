1878e9d5f89ce345da6b2396f4ce133fdbf79765
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-06-10 16:36:41 +0530

    Added solution - LeetHub

---
+++ Kth element in Matrix - GFG/kth-element-in-matrix.cpp
@@ -0,0 +1,59 @@
+// { Driver Code Starts
+// kth largest element in a 2d array sorted row-wise and column-wise
+#include<bits/stdc++.h>
+using namespace std;
+#define MAX 1000
+int mat[MAX][MAX];
+int kthSmallest(int mat[MAX][MAX], int n, int k);
+// driver program to test above function
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+    
+        for(int i=0;i<n;i++)
+            for(int j=0;j<n;j++)
+                cin>>mat[i][j];
+        int r;
+        cin>>r;
+        cout<<kthSmallest(mat,n,r)<<endl;
+    }
+     // cout << "7th smallest element is " << kthSmallest(mat, 4, 7);
+      return 0;
+}
+// } Driver Code Ends
+
+
+
+int kthSmallest(int mat[MAX][MAX], int n, int k)
+{
+  //Your code here
+  vector<int> table(n,0);
+  int count = 0;
+  priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+  for (int i = 0; i < n;i++)
+  {
+      pq.push({mat[0][i],i});
+  }
+  pair<int,int> tp;
+  while (1)
+  {
+      tp = pq.top();
+      count++;
+      if (count == k)
+      {
+          return tp.first;
+      }
+      pq.pop();
+      table[tp.second]++;
+      if (table[tp.second] != n)
+      {
+          pq.push({mat[table[tp.second]][tp.second],tp.second});
+      }
+  }
+  return 1;
+}
