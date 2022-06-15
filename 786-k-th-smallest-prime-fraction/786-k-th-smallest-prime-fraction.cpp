ddcc4ea0e973ad79bb5c9d7278c6eb4f96a92d81
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-06-15 23:36:06 +0530

    Time: 691 ms (44.27%), Space: 8.9 MB (52.20%) - LeetHub

--- 786-k-th-smallest-prime-fraction/786-k-th-smallest-prime-fraction.cpp
+++ 786-k-th-smallest-prime-fraction/786-k-th-smallest-prime-fraction.cpp
@@ -3,20 +3,28 @@ public:
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
         int n = arr.size();
         int m = n*(n - 1)/2;
-        vector<pair<double,pair<int,int>>> v(m);
-        int c = 0;
-        for (int i = 0; i < n;i++)
+        priority_queue<pair<double,pair<int,int>>,vector<pair<double,pair<int,int>>>,greater<pair<double,pair<int,int>>>> minheap;
+        for (int i = 0; i < n - 1; i++)
         {
-            for (int j = i + 1; j < n; j++)
-            {
-                v[c++].first = arr[i]/(arr[j]/1.0);
-                v[c - 1].second = {arr[i],arr[j]};
-            }
+            minheap.push({(arr[i]/(arr[n - 1]/1.0)),{i,n - 1}});
         }
-        sort(v.begin(),v.end());
+        int p1,p2;
+        pair<double,pair<int,int>> p;
+        for (int i = 0; i < k; i++)
+        {
+            p = minheap.top();
+            minheap.pop();
+            p1 = p.second.first;
+            p2 = p.second.second;
+            if (p2 > (p1 + 1))
+            {
+                minheap.push({(arr[p1]/(arr[p2 - 1]/1.0)),{p1,p2 - 1}});
+            }
+            
+        }
         vector<int> ans(2);
-        ans[0] = v[k - 1].second.first;
-        ans[1] = v[k - 1].second.second;
+        ans[0] = arr[p1];
+        ans[1] = arr[p2];
         return ans;
     }
 };
