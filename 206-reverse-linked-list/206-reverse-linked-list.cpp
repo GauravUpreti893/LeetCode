499a7b3dbc21e2fdb3140b97929c679e13d2bd9d
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-06-17 10:22:17 +0530

    Time: 11 ms (38.95%), Space: 8.3 MB (42.00%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -10,31 +10,31 @@
  */
 class Solution {
 public:
-    ListNode* rev(ListNode* curr, ListNode *&newhead)
-    {
-        if (curr == NULL || curr->next == NULL)
-        {
-            newhead = curr;
-            return curr;
-        }
-        ListNode* prev = rev(curr->next, newhead);
-        prev->next = curr;
-        return curr;
-    }
+    // ListNode* rev(ListNode* curr, ListNode *&newhead)
+    // {
+    //     if (curr == NULL || curr->next == NULL)
+    //     {
+    //         newhead = curr;
+    //         return curr;
+    //     }
+    //     ListNode* prev = rev(curr->next, newhead);
+    //     prev->next = curr;
+    //     return curr;
+    // }
     ListNode* reverseList(ListNode* head) {
-        // ListNode *prev = NULL, *curr = head, *next;
-        // while (curr != NULL)
-        // {
-        //     next = curr->next;
-        //     curr->next = prev;
-        //     prev = curr;
-        //     curr = next;
-        // }
-        // return prev;
-        ListNode *newhead;
-        ListNode *nw = rev(head, newhead);
-        if (nw != NULL)
-            nw->next = NULL;
-        return newhead;
+        ListNode *prev = NULL, *curr = head, *next;
+        while (curr != NULL)
+        {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+        // ListNode *newhead;
+        // ListNode *nw = rev(head, newhead);
+        // if (nw != NULL)
+        //     nw->next = NULL;
+        // return newhead;
     }
 };
