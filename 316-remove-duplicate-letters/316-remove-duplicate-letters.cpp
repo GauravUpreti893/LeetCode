e4d5045b3b5a322695c0b165bf8aa79a97a9f304
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-07-16 15:07:01 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (60.72%) - LeetHub

---
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        int n = s.size();
+        string ans;
+        stack<char> st;
+        vector<int> ispresent(26,0), freq(26,0);
+        for (int i = 0; i < n; i++)
+        {
+            freq[s[i] - 97]++;
+        }
+        for (int i = 0; i < n;i++)
+        {
+            freq[s[i] - 97]--;
+            if (ispresent[s[i] - 97])
+                continue;
+            while (!st.empty() && st.top() > s[i])
+            {
+                if (!freq[st.top() - 97])
+                {
+                    break;
+                }
+                ispresent[st.top() - 97] = 0;
+                st.pop();
+                
+            }
+            st.push(s[i]);
+            ispresent[s[i] - 97] = 1;
+        }
+        while (!st.empty())
+        {
+            ans = st.top() + ans;
+            st.pop();
+        }
+        return ans;
+    }
+};
