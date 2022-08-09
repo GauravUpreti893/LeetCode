64314b6664fb85f8fa6741e15dd8c62b75be154f
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-08-09 12:14:56 +0530

    Time: 5 ms (34.38%), Space: 9.2 MB (46.86%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.cpp
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.cpp
@@ -11,48 +11,17 @@
  */
 class Solution {
 public:
-    vector<string> sum(TreeNode* root)
+    int sum(TreeNode* root, int s)
     {
-        char c;
         if (root == NULL)
+            return 0;
+        if (root->left == NULL && root->right == NULL)
         {
-            vector<string> v;
-            return v;
-        } 
-        vector<string> left = sum(root->left);
-        vector<string> right = sum(root->right);
-        if (left.size() == 0 && right.size() == 0) // Leaf Node
-        {
-            vector<string> v;
-            string s;
-            c = root->val + '0';
-            s += c;
-            v.push_back(s);
-            return v;
+            return 10*s + root->val;
         }
-        int n = left.size();
-        int m = right.size();
-        vector<string> v(n + m);
-        for (int i = 0; i < n; i++)
-        {
-            c = root->val + '0';
-            v[i] += c + left[i];
-        }
-        for (int i = 0; i < m; i++)
-        {
-            c = root->val + '0';
-            v[i + n] += c + right[i];
-        }
-        return v;
+        return sum(root->left, 10*s + root->val) + sum(root->right, 10*s + root->val);
     }
     int sumNumbers(TreeNode* root) {
-        vector<string> v = sum(root);
-        int n = v.size();
-        int ans = 0;
-        for (int i = 0; i < n; i++)
-        {
-            ans += stoi(v[i]);
-        }
-        return ans;
+        return sum(root, 0);
     }
 };
