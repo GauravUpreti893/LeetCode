f170277139fb0934f41aa43b3e5688a65440eb29
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-08-15 12:18:23 +0530

    Time: 4 ms (94.12%), Space: 6 MB (64.52%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int ans = 0;
+        int n = s.size();
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == 'M')
+                ans += 1000;
+            else if (s[i] == 'D')
+                ans += 500;
+            else if (s[i] == 'C' && (i < n - 1 && s[i + 1] == 'D'))
+            {
+                ans += 400;
+                i++;
+            }
+            else if (s[i] == 'C' && (i < n - 1 && s[i + 1] == 'M'))
+            {
+                ans += 900;
+                i++;
+            }
+            else if (s[i] == 'C')
+                ans += 100;
+            else if (s[i] == 'L')
+                ans += 50;
+            else if (s[i] == 'X' && (i < n - 1 && s[i + 1] == 'L'))
+            {
+                ans += 40;
+                i++;
+            }
+            else if (s[i] == 'X' && (i < n - 1 && s[i + 1] == 'C'))
+            {
+                ans += 90;
+                i++;
+            }
+            else if (s[i] == 'X')
+                ans += 10;
+            else if (s[i] == 'V')
+                ans += 5;
+            else if (s[i] == 'I' && (i < n - 1 && s[i + 1] == 'V'))
+            {
+                ans += 4;
+                i++;
+            }
+            else if (s[i] == 'I' && (i < n - 1 && s[i + 1] == 'X'))
+            {
+                ans += 9;
+                i++;
+            }
+            else if (s[i] == 'I')
+                ans += 1;
+        }
+        return ans;
+    }
+};
