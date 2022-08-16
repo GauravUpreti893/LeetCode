c941b670e1f4366712452021d6310168840bbd7e
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-08-16 05:59:13 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (25.90%) - LeetHub

--- 1051-height-checker/1051-height-checker.cpp
+++ 1051-height-checker/1051-height-checker.cpp
@@ -1,14 +1,28 @@
 class Solution {
 public:
     int heightChecker(vector<int>& heights) {
-        vector<int> expheights = heights;
-        sort(expheights.begin(), expheights.end());
         int count = 0;
+        vector<int> freq(101, 0);
         int n = heights.size();
         for (int i = 0; i < n; i++)
         {
-            if (heights[i] != expheights[i])
-                count++;
+            freq[heights[i]]++;
+        }
+        int j = 0;
+        for (int i = 1; i <= 100; i++)
+        {
+            int m = freq[i];
+            for (int k = 0; k < m; k++)
+            {
+                if (heights[j] != i)
+                {
+                    // cout<<i<<endl;
+                    count++;
+                }
+                j++;
+                if (j == n)
+                    return count;
+            }
         }
         return count;
     }
