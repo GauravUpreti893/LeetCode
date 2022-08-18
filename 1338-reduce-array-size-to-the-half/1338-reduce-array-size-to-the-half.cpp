9a11316091a7c6d4a92aef3e580d963938bd5d08
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-08-18 07:35:56 +0530

    Time: 350 ms (49.47%), Space: 77.7 MB (87.22%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -5,20 +5,27 @@ public:
         int n = arr.size();
         for (int i = 0; i < n; i++)
         {
-            freq[arr[i]]++;
+            freq[arr[i]]++; // Finding frquency of elements present in arr
         }
-        priority_queue<int> pq;
+        vector<int> freqcount(n + 1, 0); // Using count sort, freqcount[3] means how many frequencies are actually 3. This can be used as a sorting as maximum frequency possible is n, We will iterate from n to 1 and see when we find enough elements to remove, also this will give shortest as we are starting from highest frequency.
         for (auto it = freq.begin(); it != freq.end(); it++)
         {
-            pq.push(it->second);
+            freqcount[it->second]++;
         }
         int val = n/2;
         int ans = 0;
-        while (val > 0)
+        for (int i = n; i > 0; i--)
         {
-            val -= pq.top();
-            pq.pop();
-            ans++;
+            int noOfFrequenciesRequired = ceil (val/ (i / 1.0));
+            if (noOfFrequenciesRequired > freqcount[i])
+            {
+                noOfFrequenciesRequired = freqcount[i];
+            }
+            int usedfreq = noOfFrequenciesRequired*i;
+            val -= usedfreq;
+            ans += noOfFrequenciesRequired;
+            if (val <= 0)
+                break;
         }
         return ans;
     }
