e60c86e0e989f6d4158b6663f0504e449f4ec134
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-08-24 16:19:16 +0530

    Time: 15 ms (92.16%), Space: 19.8 MB (82.05%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int r = height.size() - 1, l = 0, lmx = 0, rmx = 0, ans = 0;
+        if (height.size() < 3)
+            return 0;
         while (l <= r)
         {
             if (lmx <= rmx)
