9a426eaad95b649b343b7166f834785972741722
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-08-25 10:43:51 +0530

    Time: 90 ms (5.57%), Space: 27.6 MB (63.73%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int search(vector<int>& arr, int v) {
-          int i = 0, j = arr.size() - 1, mid;
-            while (i <= j)
+        int i = 0, j = arr.size() - 1, mid;
+        while (i <= j)
+        {
+            mid = (i + j)/2;
+            if (arr[mid] == v)
             {
-                mid = (i + j)/2;
-                if (arr[mid] == v)
-                {
-                    return mid;
-                }
-                else if (arr[mid] > v)
-                {
-                    j = mid - 1;
-                }
-                else
-                {
-                    i = mid + 1;
-                }
+                return mid;
             }
-            return -1;
+            else if (arr[mid] > v)
+            {
+                j = mid - 1;
+            }
+            else
+            {
+                i = mid + 1;
+            }
+        }
+        return -1;
     }
 };
