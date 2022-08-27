ba4576c7ad2f5661a9c3f451ccedb7696335a617
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-08-27 11:12:37 +0530

    Time: 57 ms (57.26%), Space: 25 MB (14.28%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        map<vector<int>,vector<string>> mp;
+        unordered_map<string,vector<string>> mp;
         int n = strs.size(), m;
         for (int i = 0; i < n; i++)
         {
@@ -11,7 +11,14 @@ public:
             {
                 freq[strs[i][j] - 'a']++;
             }
-            mp[freq].push_back(strs[i]);
+            string s;
+            char c;
+            for (int j = 0; j < 26; j++)
+            {
+                c = freq[j] + '0';
+                s += c;
+            }
+            mp[s].push_back(strs[i]);
         }
         vector<vector<string>> ans;
         for (auto it = mp.begin(); it != mp.end(); it++)
