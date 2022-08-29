cd755fda7f071c684dc1e4f6c619494117878166
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-08-29 06:36:29 +0530

    Time: 463 ms (74.84%), Space: 9 MB (68.29%) - LeetHub

--- 786-k-th-smallest-prime-fraction/786-k-th-smallest-prime-fraction.cpp
+++ 786-k-th-smallest-prime-fraction/786-k-th-smallest-prime-fraction.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
+        vector<int> ans(2);
         int n = arr.size();
-        int m = n*(n - 1)/2;
-        priority_queue<pair<double,pair<int,int>>,vector<pair<double,pair<int,int>>>,greater<pair<double,pair<int,int>>>> minheap;
-        for (int i = 0; i < n - 1; i++)
+        int i = 0, j = n - 1;
+        priority_queue<pair<double, int>, vector<pair<double, int>>, greater<pair<double, int>>> pq;
+        vector<int> pos(n, 0), den(n);
+        double d;
+        for (int i = 0; i < n; i++)
         {
-            minheap.push({(arr[i]/(arr[n - 1]/1.0)),{i,n - 1}});
+            d = arr[0]/(arr[i]/1.0);
+            // den[i] = i + 1;
+            pq.push({d,i});
         }
-        int p1,p2;
-        pair<double,pair<int,int>> p;
+        double v;
+        int idx;
         for (int i = 0; i < k; i++)
         {
-            p = minheap.top();
-            minheap.pop();
-            p1 = p.second.first;
-            p2 = p.second.second;
-            if (p2 > (p1 + 1))
+            v = pq.top().first;
+            idx = pq.top().second;
+            pq.pop();
+            pos[idx]++;
+            if (v != 1)
             {
-                minheap.push({(arr[p1]/(arr[p2 - 1]/1.0)),{p1,p2 - 1}});
+                d = arr[pos[idx]]/(arr[idx]/1.0);
+                pq.push({d, idx});
             }
             
         }
-        vector<int> ans(2);
-        ans[0] = arr[p1];
-        ans[1] = arr[p2];
+        ans[0] = arr[pos[idx] - 1];
+        ans[1] = arr[idx];
         return ans;
     }
 };
