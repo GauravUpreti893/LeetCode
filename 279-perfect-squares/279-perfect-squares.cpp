6c9954adabcf3414cc062b6352eb1637008f645c
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-09-01 16:28:52 +0530

    Time: 440 ms (25.43%), Space: 9.1 MB (68.58%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    int minsq(int n, vector<int> &table)
-    {
-        if (table[n] != INT_MAX)
-            return table[n];
-        for (int i = 1; i*i <= n; i++)
-        {
-            table[n] = min(table[n], minsq(n - i*i, table) + 1);
-        }
-        return table[n];
-    }
     int numSquares(int n) {
-        if (n == 0)
-        {
-            return 0;
-        }
         vector<int> table(n + 1, INT_MAX);
         table[0] = 0;
-        return minsq(n, table);
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j*j <= i; j++)
+            {
+                table[i] = min(table[i], table[i - j*j] + 1);
+            }
+        }
+        return table[n];
     }
 };
