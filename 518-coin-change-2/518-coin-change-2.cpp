421b45de0fc95790448af7f57d9453b669d22aee
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-09-02 12:00:55 +0530

    Time: 74 ms (24.61%), Space: 18.6 MB (38.83%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-    int noofways(int amount, vector<int>&coins, int n, int i, vector<vector<int>>&table)
-    {
-        if (i == n || amount < 0)
-            return 0;
-        if (table[i][amount] != -1)
-            return table[i][amount];
-        if (amount - coins[i] == 0)
-            return 1;
-        return table[i][amount] = noofways(amount, coins, n, i + 1, table) + noofways(amount - coins[i], coins, n, i, table); 
-    }
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> table(n, vector<int> (amount + 1, -1));
-        for (int i = 0; i < n; i++)
+        vector<vector<int>> table(n + 1, vector<int> (amount + 1));
+        for (int i = 1; i <= amount; i++)
+        {
+            table[0][i] = 0;
+        }
+        for (int i = 0; i <= n; i++)
         {
             table[i][0] = 1;
         }
-        return noofways(amount, coins, n, 0, table);
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= amount; j++)
+            {
+                table[i][j] = table[i - 1][j];
+                if (j - coins[i - 1] >= 0)
+                    table[i][j] += table[i][j - coins[i - 1]];
+            }
+        }
+        return table[n][amount];
     }
 };
