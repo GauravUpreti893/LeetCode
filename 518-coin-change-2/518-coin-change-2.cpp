873d66c15c434635d4e6447c52f2c18e6479a4d5
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-09-02 12:29:44 +0530

    Time: 19 ms (77.60%), Space: 7.2 MB (90.78%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -2,24 +2,16 @@ class Solution {
 public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> table(n + 1, vector<int> (amount + 1));
-        for (int i = 1; i <= amount; i++)
-        {
-            table[0][i] = 0;
-        }
-        for (int i = 0; i <= n; i++)
-        {
-            table[i][0] = 1;
-        }
-        for (int i = 1; i <= n; i++)
+        vector<int> table(amount + 1, 0);
+        table[0] = 1;
+        for (int i = 0; i < n; i++)
         {
             for (int j = 1; j <= amount; j++)
             {
-                table[i][j] = table[i - 1][j];
-                if (j - coins[i - 1] >= 0)
-                    table[i][j] += table[i][j - coins[i - 1]];
+                if (j - coins[i] >= 0)
+                table[j] += table[j - coins[i]];
             }
         }
-        return table[n][amount];
+        return table[amount];
     }
 };
