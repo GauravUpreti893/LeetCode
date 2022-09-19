569297703920a6b9469d1c329097f22bd0cd9c8d
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-09-19 09:48:45 +0530

    Time: 60 ms (18.88%), Space: 13 MB (57.54%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
-    int longest(int n, int m, vector<vector<int>> &table, string &text1, string &text2, int idx, int idx1)
-    {
-        if (idx == n || idx1 == m)
-            return 0;
-        if (table[idx][idx1] != -1)
-            return table[idx][idx1];
-        int count = 0;
-        if (text1[idx] == text2[idx1])
-        {
-            count++;
-            count += longest(n, m, table, text1, text2, idx + 1, idx1 + 1);
-        }
-        else
-        {
-            count = max(longest(n, m ,table, text1, text2, idx + 1, idx1), longest(n, m, table, text1, text2, idx, idx1 + 1));
-        }
-        return table[idx][idx1] = count;
-    }
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.size(), m = text2.size();
-        vector<vector<int>> table(n, vector<int> (m, -1));
-        return longest(n, m, table, text1, text2, 0, 0);
+        vector<vector<int>> table(n + 1, vector<int> (m + 1, 0));
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                if (text1[i - 1] == text2[j - 1])
+                {
+                    table[i][j] = table[i - 1][j - 1] + 1;
+                }
+                else
+                {
+                    table[i][j] = max(table[i - 1][j], table[i][j - 1]);
+                }
+            }
+        }
+        return table[n][m];
     }
 };
