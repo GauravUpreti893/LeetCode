71e0c414f0cf795933541c457be8467d9c70ba63
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-09-20 18:27:12 +0530

    Time: 2174 ms (5.01%), Space: 109.2 MB (14.62%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
+    int find(vector<int>&nums1, vector<int>&nums2, int i, int j, vector<vector<int>> &table, int &ans)
+    {
+        if (i < 0 || j < 0)
+            return 0;
+        if (table[i][j] != -1)
+            return table[i][j];
+        if (nums1[i] == nums2[j])
+        {
+            table[i][j] = find(nums1, nums2, i - 1, j - 1, table, ans) + 1;
+            ans = max(ans, table[i][j]);
+        }
+        else
+        {
+            table[i][j] = 0;
+        }
+        find(nums1, nums2, i, j - 1, table, ans);
+        find(nums1, nums2, i - 1, j, table, ans);
+        return table[i][j];
+    }
     int findLength(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size(), m = nums2.size();
-        vector<vector<int>> table(n + 1, vector<int> (m + 1, 0));
-        int mx = 0;
-        for (int i = 1; i <= n; i++)
-        {
-            for (int j = 1; j <= m; j++)
-            {
-                if (nums1[i - 1] == nums2[j - 1])
-                {
-                    table[i][j] = table[i - 1][j - 1] + 1;
-                    mx = max(mx, table[i][j]);
-                }
-                else
-                {
-                    table[i][j] = 0;
-                }
-            }
-        }
-        return mx;
+        vector<vector<int>> table(n, vector<int> (m, -1));
+        int ans = 0;
+        find(nums1, nums2, n - 1, m - 1, table, ans);
+        return ans;
     }
 };
