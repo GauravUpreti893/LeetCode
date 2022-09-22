88fd4009ad962ba1baec40a811f40e52bc9fd2bd
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-09-22 06:35:13 +0530

    Time: 11 ms (61.59%), Space: 7.8 MB (62.85%) - LeetHub

--- 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.cpp
@@ -1,38 +1,21 @@
 class Solution {
 public:
     string reverseVowels(string s) {
-        string str;
         int n = s.size();
-        if (n == 1)
-            return s;
-        for (int i = 0; i < n; i++)
-        {
-            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U' )
-            {
-                str += s[i];
-            }
-        }
-        int m = str.size();
-        if (m == 1)
-            return s;
-        int i = 0, j = m - 1;
-        char temp;
+        int i = 0, j = n - 1;
         while (i < j)
         {
-            temp = str[i];
-            str[i] = str[j];
-            str[j] = temp;
+            while (i < j && !(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U'))
+            {
+                i++;
+            }
+            while (j > i && !(s[j] == 'a' || s[j] == 'e' || s[j] == 'i' || s[j] == 'o' || s[j] == 'u' || s[j] == 'A' || s[j] == 'E' || s[j] == 'I' || s[j] == 'O' || s[j] == 'U'))
+            {
+                j--;
+            }
+            swap(s[i], s[j]);
             i++;
             j--;
-        }
-        int count = 0;
-        for (int i = 0; i < n; i++)
-        {
-            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U' )
-            {
-                s[i] = str[count];
-                count++;
-            }
         }
         return s;
     }
