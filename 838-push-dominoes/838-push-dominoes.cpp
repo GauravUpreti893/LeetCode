ba967a5685c810235cc4c81751857b37bf8c2d76
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-09-27 08:31:37 +0530

    Time: 124 ms (16.12%), Space: 16.7 MB (62.91%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.cpp
+++ 838-push-dominoes/838-push-dominoes.cpp
@@ -1,65 +1,55 @@
 class Solution {
 public:
     string pushDominoes(string dominoes) {
-        stack<char> right;
-        char left = ' ';
-        int n = dominoes.size();
-        for (int i = n - 1; i >= 0; i--)
-        {
-            if (dominoes[i] != '.')
-            {
-                right.push(dominoes[i]);
-            }
-        }
+        int n = dominoes.size(), p;
+        char c = ' ';
+        vector<int> dis(n);
         for (int i = 0; i < n; i++)
         {
-            if (dominoes[i] == '.')
+            if (dominoes[i] == '.' && c != '.')
             {
-                if (left != ' ' && !right.empty())
+                if (c == 'R')
                 {
-                    if (left == right.top())
-                        dominoes[i] = left;
+                    dominoes[i] = c;
+                    dis[i] = i - p;
                 }
-                else if (left != ' ' || !right.empty())
+                else
                 {
-                    if ((left == 'R') || (!right.empty() && right.top() == 'L'))
-                        dominoes[i] = left != ' '?left:right.top();
+                    dis[i] = n + 1;
+                }
+            }
+            else if (dominoes[i] == '.' && c == '.')
+            {
+                dis[i] = n + 1;
+            }
+            else if (dominoes[i] != '.')
+            {
+                c = dominoes[i];
+                p = i;
+                dis[i] = 0;
+            }
+        }
+        c = ' ';
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (dis[i])
+            {
+                if (c == 'L')
+                {
+                    if (dis[i] == p - i)
+                    {
+                        dominoes[i] = '.';
+                    }
+                    else if (dis[i] > p - i)
+                    {
+                        dominoes[i] = 'L';
+                    }
                 }
             }
             else
             {
-                left = dominoes[i];
-                right.pop();
-                int count = 0;
-                int j = i + 1;
-                while (j < n && dominoes[j] == '.')
-                {
-                    count++;
-                    j++;
-                }
-                if ((j == n && dominoes[i] == 'R') ||  (j != n && (dominoes[j] == dominoes[i])))
-                {
-                    for (int k = i + 1; k < j; k++)
-                        dominoes[k] = dominoes[i];
-                }
-                else if (dominoes[i] == 'R' && dominoes[j] == 'L')
-                {
-                    int k = i + 1, c = 0;
-                    while (c != count/2)
-                    {
-                        dominoes[k++] = 'R';
-                        c++;
-                    }
-                    c = 0;
-                    if (count % 2)
-                        k++;
-                    while (c != count/2)
-                    {
-                        dominoes[k++] = 'L';
-                        c++;
-                    }
-                }
-                i = j - 1;
+                c = dominoes[i];
+                p = i;
             }
         }
         return dominoes;
