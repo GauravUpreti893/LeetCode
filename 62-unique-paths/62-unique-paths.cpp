f6614d3b82218a9c2f26121aa7fe4516a42776f1
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-09-30 23:58:40 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (63.70%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> table(m + 1, vector<int> (n + 1, 0));
+        vector<vector<int>> table(2, vector<int> (n + 1, 0));
         table[0][1] = 1;
         for (int i = 1; i <= m; i++)
         {
             for (int j = 1; j <= n; j++)
             {
-                table[i][j] = table[i - 1][j] + table[i][j - 1];
+                table[i%2][j] = table[!(i%2)][j] + table[i%2][j - 1];
             }
         }
-        return table[m][n];
+        return table[m%2][n];
     }
 };
