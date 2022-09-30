de3c51981e93a3d0a741a02d30b51ee560cdc176
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-10-01 00:03:04 +0530

    Time: 9 ms (24.47%), Space: 7.7 MB (67.35%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    int npaths(int a, int b, int i, int j, vector<vector<int>> &table, vector<vector<int>>& obstacleGrid)
-    {
-        if (i == a || j == b || obstacleGrid[i][j] == 1)
-        return 0;
-        int ans = 0;
-        if (table[i][j] != -1)
-        {
-            return table[i][j];
-        }
-        ans += npaths(a, b, i, j + 1, table, obstacleGrid);
-        ans += npaths(a, b, i + 1, j, table, obstacleGrid);
-        return table[i][j] = ans;
-    }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-        vector<vector<int>> table(m, vector<int> (n, - 1));
-        table[m - 1][n - 1] = 1;
-        return npaths(m, n, 0, 0, table, obstacleGrid);
+        vector<vector<int>> table(2, vector<int> (n + 1, 0));
+        table[0][1] = 1;
+        for (int i = 1; i <= m; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                table[i%2][j] = table[!(i%2)][j] + table[i%2][j - 1];
+                if (obstacleGrid[i - 1][j - 1])
+                    table[i%2][j] = 0;
+            }
+        }
+        return table[m%2][n];
     }
     
 };
