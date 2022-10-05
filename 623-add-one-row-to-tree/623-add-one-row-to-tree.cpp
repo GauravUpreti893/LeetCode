4b62b4236a92313e6ff16a39e0b6b2f17dd2b251
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-10-05 08:52:01 +0530

    Time: 39 ms (34.83%), Space: 24.9 MB (86.98%) - LeetHub

--- 623-add-one-row-to-tree/623-add-one-row-to-tree.cpp
+++ 623-add-one-row-to-tree/623-add-one-row-to-tree.cpp
@@ -11,43 +11,25 @@
  */
 class Solution {
 public:
+    bool check;
+    Solution(){
+        check = false;
+    }
     TreeNode* addOneRow(TreeNode* root, int val, int depth) {
-        queue<TreeNode*> q;
-        q.push(root);
-        int n;
-        TreeNode* top;
         if (depth == 1)
         {
-            TreeNode* newhead = new TreeNode(val);
-            newhead->left = root;
-            return newhead;
+            TreeNode* newnode = new TreeNode(val);
+            if (!check)
+            newnode->left = root;
+            else
+                newnode->right = root;
+            check = !check;
+            return newnode;
         }
-        for (int i = 1; i < depth - 1; i++)
-        {
-            n = q.size();
-            for (int j = 0; j < n; j++)
-            {
-                top = q.front();
-                q.pop();
-                if (top->left)
-                    q.push(top->left);
-                if (top->right != NULL)
-                    q.push(top->right);
-            }
-        }
-        n = q.size();
-        for (int i = 0; i < n; i++)
-        {
-            top = q.front();
-            cout<<top->val<<endl;
-            q.pop();
-            TreeNode* newn1 = new TreeNode(val);
-            TreeNode* newn2 = new TreeNode(val);
-            newn1->left = top->left;
-            newn2->right = top->right;
-            top->left = newn1;
-            top->right = newn2;
-        }
+        if (root == NULL)
+            return NULL;
+        root->left = addOneRow(root->left, val, depth - 1);
+        root->right = addOneRow(root->right, val, depth - 1);
         return root;
     }
 };
