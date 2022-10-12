528ead61d2f899cb7bd5f8b2acdc15d17eb338b6
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-10-12 16:01:41 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (15.82%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
-    int fibo(int n, vector<int> &dp)
-    {
-        if (dp[n] != -1)
-            return dp[n];
-        return dp[n] = fibo(n - 1, dp) + fibo(n - 2, dp);
-    }
     int fib(int n) {
         if (!n)
             return 0;
-       vector<int> dp(n + 1, -1);
+        vector<int> dp(n + 1);
         dp[0] = 0;
         dp[1] = 1;
-        return fibo(n, dp);
+        for (int i = 2; i <= n; i++)
+            dp[i] = dp[i - 1] + dp[i - 2];
+        return dp[n];
     }
 };
