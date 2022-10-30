6380e46d51ecf58055b4cb5e1c3f782a1fa506db
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-10-30 17:07:01 +0530

    Time: 914 ms (12.50%), Space: 382.8 MB (12.50%) - LeetHub

---
+++ 2454-next-greater-element-iv/2454-next-greater-element-iv.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> secondGreaterElement(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n, -1);
+        stack<int> st1, st2;
+        for (int i = 0; i < n; i++)
+        {
+            while (!st2.empty() && nums[st2.top()] < nums[i])
+            {
+                ans[st2.top()] = nums[i];
+                st2.pop();
+            }
+            stack<int> temp;
+            while (!st1.empty() && nums[st1.top()] < nums[i])
+            {
+                temp.push(st1.top());
+                st1.pop();
+            }
+            while (!temp.empty())
+            {
+                st2.push(temp.top());
+                temp.pop();
+            }
+            st1.push(i);
+        }
+        return ans;
+    }
+};
