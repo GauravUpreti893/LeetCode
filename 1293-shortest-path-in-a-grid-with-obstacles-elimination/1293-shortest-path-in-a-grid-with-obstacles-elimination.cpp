366af6225955528808df7a1362482e8aa38ec4f5
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-10-31 18:03:35 +0530

    Time: 84 ms (76.91%), Space: 15.1 MB (78.15%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,49 +1,57 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>& grid,int k){
-		// At a particular cell we will store the number of obstacles that we can still remove after walking through that cell
-        vector<vector<int>> vis(grid.size(),vector<int>(grid[0].size(),-1));
+    int shortestPath(vector<vector<int>>& grid, int k) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<pair<int,int>> xy{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        vector<int> v{0,0,k, 0};
         queue<vector<int>> q;
-		
-		// queue stores (x,y,current path length,number of obstacles we can still remove)
-        q.push({0,0,0,k});
-        while(!q.empty()){
-            auto t=q.front();
-            int x=t[0],y=t[1];
+        q.push(v);
+        int x, y;
+        vector<vector<int>> visited(n, vector<int> (m, -1));
+        visited[0][0] = 1;
+        while (!q.empty())
+        {
+            v = q.front();
             q.pop();
-			
-			// Exit if current position is outside of the grid
-            if(x<0 || y<0 || x>=grid.size() || y>=grid[0].size()){
-                continue;
+            if (v[0] == n -1 && v[1] == m - 1)
+            {
+                return v[3];
             }
-			
-			// Destination found
-            if(x==grid.size()-1 && y==grid[0].size()-1)
-                return t[2];
-
-            if(grid[x][y]==1){
-                if(t[3]>0) // If we encounter an obstacle and we can remove it
-                    t[3]--;
-                else
+            for (int i = 0; i < 4; i++)
+            {
+                x = v[0] + xy[i].first;
+                y = v[1] + xy[i].second;
+                int x1 = v[2];
+                if (x < 0 || y < 0 || x == n || y == m)
                     continue;
+                if (grid[x][y] == 1)
+                {
+                    if (v[2] > 0)
+                    {
+                       x1--;
+                    }
+                    else
+                        continue;
+                }
+                if ((visited[x][y] != -1 &&  visited[x][y] >= x1))
+                {
+                    continue;
+                }
+                visited[x][y] = x1;
+                if (grid[x][y] == 1)
+                {
+                    if (v[2] > 0)
+                    {
+                        q.push({x, y, v[2] - 1, v[3] + 1});
+                    }
+                    else
+                        continue;
+                }
+                else
+                q.push({x, y, v[2], v[3] + 1});
             }
-			
-			// The cell was previously visited by some path and we were able to remove more obstacles from the previous path.
-			// Then we don't need to continue on our current path
-            if(vis[x][y]!=-1 && vis[x][y]>=t[3])
-                continue;
-            vis[x][y]=t[3];
-            
-            q.push({x+1,y,t[2]+1,t[3]});
-            q.push({x,y+1,t[2]+1,t[3]});
-            q.push({x-1,y,t[2]+1,t[3]});
-            q.push({x,y-1,t[2]+1,t[3]});
-            
         }
         return -1;
-    }
-    
-    int shortestPath(vector<vector<int>>& grid, int k) {
-        return solve(grid,k);
     }
 };
