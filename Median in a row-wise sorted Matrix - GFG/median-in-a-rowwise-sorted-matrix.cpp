3ea79e3765c2ecc294829e5a5161659d7d229431
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-11-01 23:54:40 +0530

    Added solution - LeetHub

--- Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.cpp
+++ Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.cpp
@@ -1,59 +1,46 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 //Initial template for C++
 
 #include <bits/stdc++.h>
 using namespace std;
 
 
- // } Driver Code Ends
+// } Driver Code Ends
 //User function template for C++
 
 class Solution{   
 public:
-    int count(int v,vector<int>&arr)
-    {
-        int n = arr.size();
-        int i = 0,j = n - 1, mid;
-        while (i <= j)
-        {
-            mid = (i + j)/2;
-            if (arr[mid] <= v)
-            {
-                i = mid + 1;
+    int median(vector<vector<int>> &a, int n, int m) {
+        // code here
+        //eg: 1 2 3 3    3    4 5 7 10    median will have 4 elements to its left
+        //if countSmaller(x) > 4 then x cannot be ans but if it is <= 4 it might be ans 
+        int low = 1, high = 2000, req = (n * m) / 2 ;
+        int ans = -1;
+    
+        auto countSmaller = [&](int val) {
+            int smaller = 0;
+            for (int i = 0; i < n; i++) {
+                smaller += lower_bound(a[i].begin(), a[i].end(), val) - a[i].begin();
+            }
+            return smaller;
+        };
+    
+        while (low <= high) {
+            int mid = (low + high) >> 1;
+            int smallerElements = countSmaller(mid);
+            if (smallerElements <= req) {
+                ans = mid;
+                low = mid + 1;
             }
             else
-            {
-                j = mid - 1;
-            }
+                high = mid - 1;
         }
-        return i;
-    }
-    int median(vector<vector<int>> &matrix, int r, int c){
-        // code here    
-        int i = 1, j = 2000;
-        int mid,n = 0, midpos = (r*c)/2;
-        while (i <= j)
-        {
-            mid = (i + j)/2;
-            n = 0;
-            for (int k = 0; k < r; k++)
-            {
-                n += count(mid, matrix[k]);
-            }
-            if (n <= midpos)
-            {
-                i = mid + 1;
-            }
-            else
-            {
-                j = mid - 1;
-            }
-        }
-        return i;
+        return ans;
     }
 };
 
-// { Driver Code Starts.
+
+//{ Driver Code Starts.
 
 int main()
 {
@@ -71,4 +58,5 @@ int main()
         cout<<obj.median(matrix, r, c)<<endl;        
     }
     return 0;
-}  // } Driver Code Ends
+}
+// } Driver Code Ends
