6cf99a556d938a73a6fbb1598013fb64054b618e
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-11-03 10:33:47 +0530

    Time: 36 ms (70.99%), Space: 13.2 MB (50.36%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -1,20 +1,36 @@
 class Solution {
 public:
-     vector<int> findBall(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        vector<int> res;
-        for (int i = 0; i < n; ++i) {
-            int i1 = i, i2;
-            for (int j = 0; j < m; ++j) {
-                i2 = i1 + grid[j][i1];
-                if (i2 < 0 || i2 >= n || grid[j][i2] != grid[j][i1]) {
-                    i1 = -1;
+    vector<int> findBall(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<int> ans(m);
+        for (int i = 0; i < m; i++)
+        {
+            int col = i;
+            bool flag = true;
+            
+            for (int j = 0; j < n; j++)
+            {
+                // cout<<j<<" "<<col<<endl;
+                if ((grid[j][col] == 1 && (col < m - 1 && grid[j][col + 1] == -1)) || (grid[j][col] == -1 && (col > 0 && (grid[j][col - 1]) == 1)))
+                {
+                    
+                    ans[i] = -1;
+                    flag = false;
                     break;
                 }
-                i1 = i2;
+                int temp = col;
+                col += grid[j][temp]; 
+                if (col < 0 || col >= m)
+                {
+                    ans[i] = -1;
+                    flag = false;
+                    break;
+                }
             }
-            res.push_back(i1);
+            if (flag)
+            ans[i] = col;
         }
-        return res;
+        return ans;
     }
 };
