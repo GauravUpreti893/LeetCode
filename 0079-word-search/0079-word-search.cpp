f142b34d3b57ba3093d4cb6f92d1e32b93c4583c
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-11-05 15:54:39 +0530

    Time: 746 ms (47.08%), Space: 8.1 MB (47.23%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,41 +1,24 @@
 class Solution {
 public:
-    bool doexist(int i, int j, string &word, int idx, vector<vector<char>> &board)
-    {
-        if (i == board.size() || i < 0 || j == board[0].size() || j < 0 || board[i][j] != word[idx])
-        {
-            return false;
-        }
-        vector<pair<int, int>> xy{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        char c = board[i][j];;
-        int x, y;
-        idx++;
-        if (idx == word.size())
-            return true;
-        board[i][j] = '0';
-        for (int k = 0; k < 4; k++)
-        {
-            x = i + xy[k].first;
-            y = j + xy[k].second;
-            bool a = doexist(x, y, word, idx, board);
-            if (a)
-                return true;
-        }
-        board[i][j] = c;
-        return false;
-    }
     bool exist(vector<vector<char>>& board, string word) {
-        int n = word.size();
-        int n1 = board.size(), n2 = board[0].size();
-        for (int i = 0; i < n1; i++)
-        {
-            for (int j = 0; j < n2; j++)
-            {
-                bool a = doexist(i, j, word, 0, board);
-                if (a)
-                    return true;
-            }
-        }
+    for (unsigned int i = 0; i < board.size(); i++) 
+        for (unsigned int j = 0; j < board[0].size(); j++) 
+            if (dfs(board, i, j, word))
+                return true;
+    return false;
+}
+
+bool dfs(vector<vector<char>>& board, int i, int j, string& word) {
+    if (!word.size())
+        return true;
+    if (i<0 || i>=board.size() || j<0 || j>=board[0].size() || board[i][j] != word[0])  
         return false;
-    }
+    char c = board[i][j];
+    board[i][j] = '*';
+    string s = word.substr(1);
+    bool ret = dfs(board, i-1, j, s) || dfs(board, i+1, j, s) || dfs(board, i, j-1, s) || dfs(board, i, j+1, s);
+    board[i][j] = c;
+    return ret;
+}
+    
 };
