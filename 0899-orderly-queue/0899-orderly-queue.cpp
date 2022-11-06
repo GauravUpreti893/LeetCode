ff6aadb8fe3ff0fb3a81a12dd54629fbb2089d89
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-11-06 10:39:09 +0530

    Time: 15 ms (16.67%), Space: 12 MB (12.07%) - LeetHub

--- 0899-orderly-queue/0899-orderly-queue.cpp
+++ 0899-orderly-queue/0899-orderly-queue.cpp
@@ -3,25 +3,13 @@ public:
     string orderlyQueue(string s, int k) {
         if (k == 1)
         {
-            char mn = s[0];
             int n = s.size();
-            for (int i = 1; i < n; i++)
-            {
-               mn = min(mn, s[i]);
-            }
             string ans = s;
             for (int i = 0; i < n; i++)
             {
-                if (s[i] == mn)
-                {
-                    string str;
-                    for (int j = i; j < n; j++)
-                        str += s[j];
-                    for (int j = 0; j < i; j++)
-                        str += s[j];
-                    if (ans.compare(str) > 0)
-                        ans = str;
-                }
+                s = s.substr(1) + s[0];
+                if (s.compare(ans) < 0)
+                    ans = s;
             }
             return ans;
         }
