787ae193fe240b079195b8c8e6c2b5d13e588059
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-11-13 11:08:02 +0530

    Added solution - LeetHub

--- Fill up buckets - GFG/fill-up-buckets.cpp
+++ Fill up buckets - GFG/fill-up-buckets.cpp
@@ -12,10 +12,11 @@ public:
     int totalWays(int n, vector<int>capacity) {
         // code here
         sort(capacity.begin(), capacity.end());
-        long long ans = 1, sub  = 0, mod = 1e9 + 7;
+        long long ans = 1, sub  = 0, mod = 1e9 + 7, x;
         for (int i = 0; i < n; i++)
         {
-            ans *= capacity[i] - sub;
+            x = capacity[i] - sub;
+            ans *= x;
             ans %= mod;
             sub++;
         }
