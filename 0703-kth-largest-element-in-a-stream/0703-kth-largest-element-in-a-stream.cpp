bc53c0f5c5324bb172aced79381c0b1465290e99
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-11-14 23:27:09 +0530

    Time: 100 ms (9.69%), Space: 22.2 MB (5.30%) - LeetHub

---
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
@@ -0,0 +1,50 @@
+class KthLargest {
+public:
+    multiset<int, greater<int>> st;
+    multiset<int, greater<int>> :: iterator it;
+    int size, K;
+    KthLargest(int k, vector<int>& nums) {
+        int n = nums.size();
+        size = min(n, k);
+        K = k;
+        for (int i = 0; i < n; i++)
+        {
+            st.insert(nums[i]);
+        }
+        if (n != 0)
+        it = st.begin();
+        for (int i = 0; i < size - 1; i++)
+        {
+            it++;
+        }
+    }
+    
+    int add(int val) {
+        st.insert(val);
+        if (size != K)
+        {
+            if (size)
+            {
+                if (*it >= val)
+                {
+                    it++;
+                }
+            }
+            else
+                it = st.begin();
+            size++;
+            return *it;
+        }
+        if (val > *it)
+        {
+            it = prev(it);
+        }
+        return *it;
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
