754b203e53cab4bedfbef69a22ff2c7690d627ff
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-11-23 08:56:56 +0530

    Time: 62 ms (20.06%), Space: 22 MB (5.26%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -2,27 +2,20 @@ class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
         vector<unordered_map<char, int>> row(9), col(9);
-        for (int k = 0; k < 9; k += 3)
+        vector<vector<unordered_map<char, int>>> mp(3, vector<unordered_map<char, int>>(3));
+        for (int i = 0; i < 9; i++)
         {
-            for (int l = 0; l < 9; l += 3)
+            for (int j = 0; j < 9; j++)
             {
-                unordered_map<char, int> mp;
-                for (int i = k; i < k + 3; i++)
-                {
-                    for (int j = l; j < l + 3; j++)
-                    {
-                        if (board[i][j] == '.')
-                            continue;
-                        if (mp[board[i][j]] == 1 || row[i][board[i][j]] == 1 || col[j][board[i][j]] == 1)
-                            return false;
-                        mp[board[i][j]] = 1;
-                        row[i][board[i][j]] = 1;
-                        col[j][board[i][j]] = 1;
-                    }
-                }
+                if (board[i][j] == '.')
+                    continue;
+                if (mp[(i/3)][j/3][board[i][j]] == 1 || row[i][board[i][j]] == 1 || col[j][board[i][j]] == 1)
+                    return false;
+                mp[i/3][j/3][board[i][j]] = 1;
+                row[i][board[i][j]] = 1;
+                col[j][board[i][j]] = 1;
             }
         }
-        
         return true;
     }
 };
