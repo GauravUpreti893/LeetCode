9d2317643e1a910af5c94c67fc468e92bcfc8ec0
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-11-23 08:58:21 +0530

    Time: 76 ms (6.91%), Space: 22.4 MB (5.26%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        vector<unordered_map<char, int>> row(9), col(9);
-        vector<vector<unordered_map<char, int>>> mp(3, vector<unordered_map<char, int>>(3));
+        vector<unordered_map<char, int>> row(9), col(9), mp(23);
         for (int i = 0; i < 9; i++)
         {
             for (int j = 0; j < 9; j++)
             {
                 if (board[i][j] == '.')
                     continue;
-                if (mp[(i/3)][j/3][board[i][j]] == 1 || row[i][board[i][j]] == 1 || col[j][board[i][j]] == 1)
+                if (mp[(i/3)*10 + j/3][board[i][j]] == 1 || row[i][board[i][j]] == 1 || col[j][board[i][j]] == 1)
                     return false;
-                mp[i/3][j/3][board[i][j]] = 1;
+                mp[(i/3)*10 + j/3][board[i][j]] = 1;
                 row[i][board[i][j]] = 1;
                 col[j][board[i][j]] = 1;
             }
