0970eabaadc1ec473458a0133e5ea874ef145d95
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-11-24 22:34:25 +0530

    Time: 295 ms (90.12%), Space: 7.9 MB (94.48%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
-    bool exist(vector<vector<char>>& board, string word) {
-    for (unsigned int i = 0; i < board.size(); i++) 
-        for (unsigned int j = 0; j < board[0].size(); j++) 
-            if (dfs(board, i, j, word))
-                return true;
-    return false;
-}
-
-bool dfs(vector<vector<char>>& board, int i, int j, string& word) {
-    if (!word.size())
-        return true;
-    if (i<0 || i>=board.size() || j<0 || j>=board[0].size() || board[i][j] != word[0])  
+    bool doexist(int n, int m, vector<vector<char>>& board, string& word, int idx)
+    {
+        if ((n < 0) || (m < 0) || (n == board.size()) || (m == board[0].size()) || (board[n][m] != word[idx]))
+            return false;
+        char c = board[n][m];
+        board[n][m] = '0';
+        idx++;
+        if (idx == word.size())
+          return true;  
+        if (doexist(n + 1, m, board, word, idx) || doexist(n - 1, m, board, word, idx) || doexist(n, m + 1, board, word, idx) || doexist(n, m - 1, board, word, idx))
+            return true;
+        board[n][m] = c;
         return false;
-    char c = board[i][j];
-    board[i][j] = '*';
-    string s = word.substr(1);
-    bool ret = dfs(board, i-1, j, s) || dfs(board, i+1, j, s) || dfs(board, i, j-1, s) || dfs(board, i, j+1, s);
-    board[i][j] = c;
-    return ret;
-}
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        int n = board.size(), m = board[0].size();
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if(doexist(i, j, board, word, 0))
+                    return true;
+            }
+        }
+        return false;
+    }
 };
