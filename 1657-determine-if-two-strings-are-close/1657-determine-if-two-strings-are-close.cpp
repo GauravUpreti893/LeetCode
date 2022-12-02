2948a5553db174eb46eac607f86d915e86a663df
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-12-02 10:49:28 +0530

    Time: 98 ms (89.22%), Space: 20.6 MB (64.37%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -4,30 +4,21 @@ public:
         int n = word1.size(), m = word2.size();
         if (n != m)
             return false;
-        unordered_map<char,int> mp1, mp2;
+        vector<int> f1(26, 0), f2(26, 0);
         for (int i = 0; i < n; i++)
         {
-            mp1[word1[i]]++;
-            mp2[word2[i]]++;
+            f1[word1[i] - 'a']++;
+            f2[word2[i] - 'a']++;
         }
         for (int i = 0; i < 26; i++)
         {
-            if ((mp1['a' + i] && !mp2['a' + i]) || (!mp1['a' + i]) && mp2['a' + i])
+            if ((!f1[i] && f2[i]) || (f1[i] && !f2[i]))
                 return false;
         }
-        unordered_map<int ,int> mp;
-        for (auto it = mp2.begin(); it != mp2.end(); it++)
-        {
-            mp[it->second]++;
-        }
-        for (auto it = mp1.begin(); it != mp1.end(); it++)
-        {
-            if (mp.find(it->second) == mp.end())
-                return false;
-            mp[it->second]--;
-            if (!mp[it->second])
-            mp.erase(it->second);
-        }
-        return true;
+        sort(f1.begin(), f1.end());
+        sort(f2.begin(), f2.end());
+        if (f1 == f2)
+            return true;
+        return false;
     }
 };
