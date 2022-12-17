1f6fb0443bba8b302a7e498a3033a599970d8499
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-12-17 08:46:47 +0530

    Added solution - LeetHub

--- Break a number - GFG/break-a-number.cpp
+++ Break a number - GFG/break-a-number.cpp
@@ -11,9 +11,7 @@ public:
     int waysToBreakNumber(int N){
         // code here 
         long long ans = 0, x = N + 2, mod = 1e9 + 7;
-        ans = (x)*(x - 1);
-        ans %= mod;
-        ans /= 2;
+        ans = (x)*(x - 1)/2;
         ans %= mod;
         return ans;
     }
