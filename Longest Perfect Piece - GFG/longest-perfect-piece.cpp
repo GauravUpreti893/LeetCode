d85306ff061668fae7bd7b43ec502baa0956cbe1
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-12-21 15:18:44 +0530

    Added solution - LeetHub

--- Longest Perfect Piece - GFG/longest-perfect-piece.cpp
+++ Longest Perfect Piece - GFG/longest-perfect-piece.cpp
@@ -8,22 +8,22 @@ class Solution {
     int longestPerfectPiece(int arr[], int N) {
         // code here
         multiset<int> st;
-        int ans = 0;
-        set<int> :: iterator it;
-        set<int> :: reverse_iterator itr;
-        int start = 0;
-        for (int i = 0; i < N; i++)
+        st.insert(arr[0]);
+        int ans = 1;
+        int j = 0;
+        for (int i = 1; i < N; i++)
         {
             st.insert(arr[i]);
-            it = st.begin();
-            itr = st.rbegin();
+            auto it = st.begin();
+            auto itr = st.rbegin();
             while (*itr - *it > 1)
             {
-                st.erase(st.find(arr[start++]));
+                st.erase(st.find(arr[j++]));
                 it = st.begin();
                 itr = st.rbegin();
             }
-            ans = max(ans, i - start + 1);
+            int m = st.size();
+            ans = max(ans, m);
         }
         return ans;
     }
