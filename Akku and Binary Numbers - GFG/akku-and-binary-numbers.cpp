b461b90995210bf2a3edeabea932d5f431c38560
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-12-26 19:56:06 +0530

    Added solution - LeetHub

--- Akku and Binary Numbers - GFG/akku-and-binary-numbers.cpp
+++ Akku and Binary Numbers - GFG/akku-and-binary-numbers.cpp
@@ -5,41 +5,36 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution{
-public:
+public: 
+    vector<long long> value;
     void precompute()
     {
         // Code Here
-    }
-    
-    long long solve(long long l, long long r){
-        // Code Here
-        long long ans = 0, n;
-        vector<long long> value(60, 0);
-        n = 1;
+        value = vector<long long> (60);
+        long long n = 1;
         for (int i = 59; i >= 0; i--)
         {
             value[i] = n;
             n *= 2;
         }
+    }
+    
+    long long solve(long long l, long long r){
+        // Code Here
+        long long ans = 0, n;
         for (int i = 0; i < 60; i++)
         {
-            // s[i] = '1';
             for (int j = i + 1; j < 60; j++)
             {
-                // s[j] = '1';
                 for (int k = j + 1; k < 60; k++)
                 {
-                    // s[k] = '1';
                     n = value[i] + value[j] + value[k];
                     if (n >= l && n <= r)
                     {
                         ans++;
                     }
-                    // s[k] = '0';
                 }
-                // s[j] = '0';
             }
-            // s[i] = '0';
         }
         return ans;
     }
