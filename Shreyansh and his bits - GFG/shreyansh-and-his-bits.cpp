e5d11cbdd31058ec0bf737ddb05061d0cabac5f3
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2023-01-06 16:15:24 +0530

    Added solution - LeetHub

--- Shreyansh and his bits - GFG/shreyansh-and-his-bits.cpp
+++ Shreyansh and his bits - GFG/shreyansh-and-his-bits.cpp
@@ -1,49 +1,45 @@
 //{ Driver Code Starts
 //Initial Template for C++
 
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 
 // } Driver Code Ends
 //User function Template for C++
-#include<vector>
-vector<vector<long long>>dp(40, vector<long long>(40, -1));
+
 class Solution{
 public:
-    
-    long long ncr(int n, int r)
+    vector<vector<long long>> dp;
+    Solution()
     {
+        dp = vector<vector<long long>> (40, (vector<long long> (40, -1)));
+    }
+    long long ncr(long long n, long long r)
+    {
+        if (n < 0 || n < r)
+        return 0;
         if (n == r || !r)
         return 1;
-        if (n < r || n < 0)
-        return 0;
         if (dp[n][r] != -1)
         return dp[n][r];
         return dp[n][r] = ncr(n - 1, r) + ncr(n - 1, r - 1);
     }
     long long count(long long x) {
         // Code Here
-       for (int i = 0; i < 40; i++)
-       {
-           for (int j = 0; j < 40; j++)
-           {
-               dp[i][j] = -1;
-           }
-       }
-       long long ans = 0;
-       int n = 0, r = 0;
-       while (x)
-       {
-           if (x&1)
-           {
-              r++; 
-              ans += ncr(n, r);
-           }
-           n++;
-           x >>= 1;
-       }
-       return ans;
+        int n = 0, one = 0;
+        long long ans = 0;
+        while (x)
+        {
+            if (x & 1)
+            {
+                one++;
+                ans += ncr(n, one);
+            }
+            n++;
+            x >>= 1;
+        }
+        return ans;
     }
 };
 
