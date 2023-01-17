142f853879bb5f681f2d31ecd1feb1eae6b88609
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2023-01-17 17:42:44 +0530

    Added solution - LeetHub

--- Count the Substring - GFG/count-the-substring.cpp
+++ Count the Substring - GFG/count-the-substring.cpp
@@ -5,64 +5,37 @@ using namespace std;
 
 // } Driver Code Ends
 //User function Template for C++
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less_equal<int>,
+             rb_tree_tag,
+             tree_order_statistics_node_update>
+    ordered_set;
 class Solution{
 public:
-    long long countInversion(vector<long long> &arr, int lo, int hi)
-    {
-        if (lo >= hi)
-        return 0;
-        int mid = lo + (hi - lo)/2;
-        return countInversion(arr, lo, mid) + countInversion(arr, mid + 1, hi) + merge(arr, lo, mid, hi);
-    }
-    long long merge(vector<long long> &arr, int lo, int mid, int hi)
-    {
-        vector<long long> a, b;
-        for (int i = lo; i <= mid; i++)
-        {
-            a.push_back(arr[i]);
-        }
-        for (int i = mid + 1; i <= hi; i++)
-        {
-            b.push_back(arr[i]);
-        }
-        int n = a.size(), m = b.size(), i = 0, j = 0, k = lo;
-        long long res = 0;
-        while ((i < n) && (j < m))
-        {
-            if (a[i] < b[j])
-            {
-                res += m - j;
-                arr[k++] = a[i++];
-            }
-            else
-            {
-                arr[k++] = b[j++];
-            }
-        }
-        while (i < n)
-        {
-            arr[k++] = a[i++];
-        }
-        while (j < m)
-        {
-            arr[k++] = b[j++];
-        }
-        return res;
-    }
   long long countSubstring(string S){
     // code here
     int n = S.size();
-    long long sum = 0;
-    vector<long long> prefixsum(n);
-    long long int count = 0;
+    vector<int> arr(n);
+    long long sum = 0, count = 0;
     for (int i = 0; i < n; i++)
     {
         sum += (S[i] == '1') ? 1 : -1;
-        prefixsum[i] = sum;
+        arr[i] = sum;
         if (sum > 0)
         count++;
     }
-    return count + countInversion(prefixsum, 0, n - 1);
+    ordered_set st;
+    st.insert(arr[0]);
+    for (int i = 1; i < n; i++)
+    {
+        st.insert(arr[i]);
+        count += st.order_of_key(arr[i]);
+    }
+    return count;
   }
 };
 
