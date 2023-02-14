d9a51cc17b486fa31025c96e9e760feb7016dc6b
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2023-02-14 08:18:07 +0530

    Added solution - LeetHub

--- Walls Coloring - GFG/walls-coloring.cpp
+++ Walls Coloring - GFG/walls-coloring.cpp
@@ -10,34 +10,19 @@ using namespace std;
 
 class Solution{   
 public:
-    int cost(vector<vector<int>> &colors, vector<vector<int>> &dp, int idx, int col)
-    {
-        int n = colors.size();
-        if (idx == n)
-        return 0;
-        if (col != -1 && dp[idx][col] != -1)
-        return dp[idx][col];
-        int ans = 1e9;
-        if (col != 0)
-        {
-            ans = min(ans, colors[idx][0] + cost(colors, dp, idx + 1, 0));
-        }
-        if (col != 1)
-        {
-            ans = min(ans, colors[idx][1] + cost(colors, dp, idx + 1, 1));
-        }
-        if (col != 2)
-        {
-            ans = min(ans, colors[idx][2] + cost(colors, dp, idx + 1, 2));
-        }
-        if (col == -1)
-        return ans;
-        return dp[idx][col] = ans;
-    }
     int minCost(vector<vector<int>> &colors, int N) {
         // Write your code here.
-        vector<vector<int>> dp(N, vector<int> (3, -1));
-        return cost(colors, dp, 0, -1);
+        vector<vector<int>> dp(N, vector<int> (3));
+        dp[0][0] = min(colors[0][1], colors[0][2]);
+        dp[0][1] = min(colors[0][0], colors[0][2]);
+        dp[0][2] = min(colors[0][0], colors[0][1]);
+        for (int i = 1; i < N; i++)
+        {
+            dp[i][0] = min(dp[i - 1][1] + colors[i][1], dp[i - 1][2] + colors[i][2]);
+            dp[i][1] = min(dp[i - 1][0] + colors[i][0], dp[i - 1][2] + colors[i][2]);
+            dp[i][2] = min(dp[i - 1][1] + colors[i][1], dp[i - 1][0] + colors[i][0]);
+        }
+        return min({dp[N - 1][0], dp[N - 1][1], dp[N - 1][2]});
     }
 };
 
