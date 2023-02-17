73b14f1d9b80fc419e18034dd107a62facf43cb1
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2023-02-17 08:51:38 +0530

    Added solution - LeetHub

--- Is it Fibonacci - GFG/is-it-fibonacci.cpp
+++ Is it Fibonacci - GFG/is-it-fibonacci.cpp
@@ -10,27 +10,23 @@ using namespace std;
 
 class Solution {
   public:
-    long long fun(int N, int K, vector<long long> &dp)
-    {
-        if (dp[N] != -1)
-        return dp[N];
-        long long ans = 0;
-        for (int i = 1; i <= K; i++)
-        {
-            ans += fun(N - i, K, dp);
-        }
-        return dp[N] = ans;
-    }
     long long solve(int N, int K, vector<long long>& GeekNum) {
         // code here
         if (N <= K)
         return GeekNum[N - 1];
-        vector<long long> dp(N + 1, -1);
+        vector<long long> dp(N + 1);
+        long long sum = 0;
         for (int i = 0; i < K; i++)
         {
             dp[i + 1] = GeekNum[i];
+            sum += GeekNum[i];
         }
-        return fun(N, K, dp);
+        for (int i = K + 1; i <= N; i++)
+        {
+            dp[i] = sum;
+            sum += sum - dp[i - K];
+        }
+        return dp[N];
     }
 };
 
