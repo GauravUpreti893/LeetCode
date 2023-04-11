98ccae0ee217ebab8cb11ec183f67cb1af4b1ed7
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2023-04-11 20:17:44 +0530

    Added solution - LeetHub

--- Minimum sum partition - GFG/minimum-sum-partition.cpp
+++ Minimum sum partition - GFG/minimum-sum-partition.cpp
@@ -12,28 +12,26 @@ class Solution{
 	    for (int i = 0; i < n; i++)
 	    sum += arr[i];
 	    int s = sum/2;
-	    vector<vector<bool>> dp(n + 1, vector<bool> (s + 1));
+	    vector<vector<bool>> dp(2, vector<bool> (s + 1));
 	    for (int i = 1; i <= s; i++)
 	    {
 	        dp[0][i] = 0;
 	    }
-	    for (int i = 0; i <= n; i++)
-	    {
-	        dp[i][0] = 1;
-	    }
+	    dp[0][0] = 1;
+	    dp[1][0] = 1;
 	    for (int i = 1; i <= n; i++)
 	    {
 	        for (int j = 1; j <= s; j++)
 	        {
-	            dp[i][j] = dp[i - 1][j];
+	            dp[i%2][j] = dp[!(i%2)][j];
 	            if (j - arr[i - 1] >= 0)
-                dp[i][j] = dp[i][j] | dp[i - 1][j - arr[i - 1]];
+                dp[i%2][j] = dp[i%2][j] | dp[!(i%2)][j - arr[i - 1]];
 	        }
 	    }
 	    int ans = 1e6;
 	    for (int i = 0; i <= s; i++)
 	    {
-	        if (dp[n][i])
+	        if (dp[n%2][i])
 	        ans = min(ans, abs(sum - 2*i));
 	    }
 	    return ans;
