1459c8002d453712d811c08b0882c34a4a761e4a
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2023-04-17 15:33:53 +0530

    Added solution - LeetHub

--- Target Sum - GFG/target-sum.cpp
+++ Target Sum - GFG/target-sum.cpp
@@ -26,7 +26,6 @@ class Solution {
         vector<vector<int>> dp(2, vector<int> (x + 1, 0));
         dp[0][0] = 1;
         dp[1][0] = 1;
-        int mod = 1e9 + 7;
         for (int i = 1; i <= n; i++)
         {
             for (int j = 0; j <= x; j++)
@@ -35,7 +34,6 @@ class Solution {
                 if (j - arr[i - 1] >= 0)
                 {
                     dp[i%2][j] += dp[!(i%2)][j - arr[i - 1]];
-                    dp[i%2][j] %= mod;
                 }
             }
         }
