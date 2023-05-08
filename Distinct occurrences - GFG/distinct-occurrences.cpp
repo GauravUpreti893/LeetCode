302d443a302c7134491bc391ddce9af6d4d4279c
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2023-05-08 16:10:42 +0530

    Added solution - LeetHub

---
+++ Distinct occurrences - GFG/distinct-occurrences.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+ 
+
+// } Driver Code Ends
+/*You are required to complete this method*/
+
+class Solution
+{
+    public:
+    int subsequenceCount(string &s, string &t)
+    {
+      //Your code here
+      int n = s.size(), m = t.size();
+        vector<int> dp(m + 1, 0);
+        int mod = 1e9 + 7;
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = m; j >= 1; j--)
+            {
+                if (s[i - 1] == t[j - 1])
+                {
+                    dp[j] += dp[j - 1];
+                    dp[j] %= mod;
+                }
+            }
+        }
+        return dp[m];
+    }
+};
+ 
+
+
+//{ Driver Code Starts. 
+
+//  Driver code to check above method
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		string s;
+		string tt;
+		cin>>s;
+		cin>>tt;
+		
+		Solution ob;
+		cout<<ob.subsequenceCount(s,tt)<<endl;
+		
+		
+	}
+  
+}
+// } Driver Code Ends
