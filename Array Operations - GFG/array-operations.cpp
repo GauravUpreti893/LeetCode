b60b4e51b6ae437587bc9a122820c73cec4a10e3
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2023-05-12 08:13:03 +0530

    Added solution - LeetHub

--- Array Operations - GFG/array-operations.cpp
+++ Array Operations - GFG/array-operations.cpp
@@ -8,17 +8,6 @@ class Solution {
   public:
     int arrayOperations(int n, vector<int> &arr) {
         // code here
-        bool flag = true;
-        // for (int i = 0; i < n; i++)
-        // {
-        //     if (!arr[i])
-        //     {
-        //         flag = false;
-        //         break;
-        //     }
-        // }
-        // if (flag)
-        // return -1;
         int i = 0;
         while (i < n && arr[i] == 0)
         i++;
