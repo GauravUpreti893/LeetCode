2b100f09302fa17a41d1477a754f0ed66d70af0b
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2023-06-08 13:18:19 +0530

    Added solution - LeetHub

--- Find Kth permutation - GFG/find-kth-permutation.cpp
+++ Find Kth permutation - GFG/find-kth-permutation.cpp
@@ -7,24 +7,52 @@ using namespace std;
 
 // } Driver Code Ends
 //User function Template for C++
-
+vector<int> fact(10, -1);
+bool flag = true;
+int factorial(int n)
+{
+    if (fact[n] != -1)
+    return fact[n];
+    return fact[n] = n * factorial(n - 1);
+}
 class Solution
 {
 public:
+set<int> st;
+    string ans;
+    int N;
+    void permut(int k)
+    {
+        int n = ans.size();
+        if (n == N)
+        return;
+        int size = fact[N - n - 1];
+        int idx = k / size;
+        auto it = st.begin();
+        for (int i = 0; i < idx; i++)
+        {
+            it++;
+        }
+        char c = *it + '0';
+        ans += c;
+        st.erase(it);
+        k %= size;
+        permut(k);
+    }
     string kthPermutation(int n, int k)
     {
         // code here
-        string s;
+       if (flag)
+        {
+            flag = false;
+            fact[0] = 1;
+            factorial(9);
+        }
         for (int i = 1; i <= n; i++)
-        {
-            char c = '0' + i;
-            s += c;
-        }
-        for (int i = 0; i < k - 1; i++)
-        {
-            next_permutation(s.begin(), s.end());
-        }
-        return s;
+        st.insert(i);
+        N = n;
+        permut(k - 1);
+        return ans;
     }
 };
 
