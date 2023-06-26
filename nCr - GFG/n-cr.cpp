7b9d74c8031d7328a694a1047db710cfc23e912d
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2023-06-26 07:46:34 +0530

    Added solution - LeetHub

--- nCr - GFG/n-cr.cpp
+++ nCr - GFG/n-cr.cpp
@@ -6,29 +6,27 @@ using namespace std;
 
 // } Driver Code Ends
 // User function Template for C++
-int mod = 1e9 + 7;
-vector<vector<int>> dp;
-bool flag = true;
+
 class Solution{
 public:
-    Solution()
-    {
-        flag = true;
-    }
     int nCr(int n, int r){
         // code here
-        if (flag)
-        {
-            flag = false;
-            dp = vector<vector<int>>(n + 1, vector<int> (r + 1, -1));
-        }
-        if (n <= 0 || n < r)
+        vector<int> prev(2, 1);
+        int mod = 1e9 + 7;
+        if (n < r)
         return 0;
-        if (n == r || r == 0)
-        return 1;
-        if (dp[n][r] != -1)
-        return dp[n][r];
-       return dp[n][r] = (nCr(n - 1, r) + nCr(n - 1, r - 1)) % mod;
+        for (int i = 2; i <= n; i++)
+        {
+            vector<int> curr = {1};
+            int m = prev.size();
+            for (int j = 0; j < m - 1; j++)
+            {
+                curr.push_back((prev[j] + prev[j + 1]) % mod);
+            }
+            curr.push_back(1);
+            prev = curr;
+        }
+        return prev[r];
     }
 };
 
