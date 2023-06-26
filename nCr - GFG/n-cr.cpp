b9122cef488479fe1453bad965e69e2145270097
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2023-06-26 07:39:53 +0530

    Added solution - LeetHub

--- nCr - GFG/n-cr.cpp
+++ nCr - GFG/n-cr.cpp
@@ -1,37 +1,38 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 // Initial Template for C++
 
 #include <bits/stdc++.h>
 using namespace std;
 
- // } Driver Code Ends
+// } Driver Code Ends
 // User function Template for C++
-
+int mod = 1e9 + 7;
+vector<vector<int>> dp;
+bool flag = true;
 class Solution{
 public:
+    Solution()
+    {
+        flag = true;
+    }
     int nCr(int n, int r){
         // code here
-        if ( r == n)
-        return 1;
-        if (r == 1)
-        return n;
-        if (r > n)
-        return 0;
-        int mod = 1e9 + 7;
-        vector<int> v(r + 1,0);
-        v[0] = 1;
-        for (int i = 0; i <= n; i++)
+        if (flag)
         {
-            for (int j = min(i,r); j > 0; j--)
-            {
-                v[j] = (v[j] + v[j - 1])%mod;
-            }
+            flag = false;
+            dp = vector<vector<int>>(n + 1, vector<int> (r + 1, -1));
         }
-         return v[r];
+        if (n <= 0 || n < r)
+        return 0;
+        if (n == r || r == 0)
+        return 1;
+        if (dp[n][r] != -1)
+        return dp[n][r];
+       return dp[n][r] = (nCr(n - 1, r) + nCr(n - 1, r - 1)) % mod;
     }
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 
 int main(){
     int t;
@@ -44,4 +45,5 @@ int main(){
         cout<<ob.nCr(n, r)<<endl;
     }
     return 0;
-}  // } Driver Code Ends
+}
+// } Driver Code Ends
