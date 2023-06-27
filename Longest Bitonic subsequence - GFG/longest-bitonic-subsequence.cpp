1c5966b7466fe1b0934f9c834e1e4304290ed6bb
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2023-06-27 20:08:45 +0530

    Added solution - LeetHub

--- Longest Bitonic subsequence - GFG/longest-bitonic-subsequence.cpp
+++ Longest Bitonic subsequence - GFG/longest-bitonic-subsequence.cpp
@@ -5,53 +5,36 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
 	public:
-	int longestincreasing(vector<int> &nums, vector<int> &increasing, int idx)
-	{
-	   // cout<<idx<<endl;
-	    if (increasing[idx] != -1)
-	    return increasing[idx];
-	    int ans = 0;
-	    for (int i = 0; i < idx; i++)
-	    {
-	       int res = longestincreasing(nums, increasing, i);
-	       if (nums[idx] > nums[i])
-	       {
-	           ans = max(ans, res);
-	       }
-	    }
-	    return increasing[idx] = ans + 1;
-	}
-	int longestdecreasing(vector<int> &nums, vector<int> &decreasing, int idx)
-	{
-	    if (decreasing[idx] != -1)
-	    return decreasing[idx];
-	    int ans = 0, n = nums.size();
-	    for (int i = idx + 1; i < n; i++)
-	    {
-	        int res = longestdecreasing(nums, decreasing, i);
-	        if (nums[idx] > nums[i])
-	        {
-	            ans = max(ans, res);
-	        }
-	    }
-	    return decreasing[idx] = ans + 1;
-	}
 	int LongestBitonicSequence(vector<int>nums)
 	{
 	    // code here
-	    int n = nums.size();
-	    vector<int> increasing(n, -1), decreasing(n, -1);
-	    increasing[0] = 1;
-	    decreasing[n - 1] = 1;
-	    longestincreasing(nums, increasing, n - 1);
-	    longestdecreasing(nums, decreasing, 0);
-	    int ans = 0;
-	    for (int i = 0; i < n; i++)
-	    {
-	       // cout<<increasing[i]<<" "<<decreasing[i]<<endl;
-	        ans = max(ans, increasing[i] + decreasing[i] - 1);
-	    }
-	    return ans;
+	    int n = nums.size(), ans = 0;
+        vector<int> inc(n, 1), dec(n, 1);
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (nums[i] > nums[j])
+                {
+                    inc[i] = max(inc[i], 1 + inc[j]);
+                }
+            }
+        }
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (nums[i] > nums[j])
+                {
+                    dec[i] = max(dec[i], 1 + dec[j]);
+                }
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            ans = max(ans, inc[i] + dec[i] - 1);
+        }
+        return ans;
 	}
 };
 
