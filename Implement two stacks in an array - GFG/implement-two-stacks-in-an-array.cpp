b221904fb70b78434f6a834a06711ba514803429
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2023-07-14 07:20:44 +0530

    Added solution - LeetHub

--- Implement two stacks in an array - GFG/implement-two-stacks-in-an-array.cpp
+++ Implement two stacks in an array - GFG/implement-two-stacks-in-an-array.cpp
@@ -1,23 +1,63 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 #include<bits/stdc++.h>
 
 using namespace std;
+
+
+// } Driver Code Ends
+
 
 class twoStacks
 {
     int *arr;
     int size;
     int top1, top2;
-public:
-   twoStacks(int n=100){size = n; arr = new int[n]; top1 = -1; top2 = size;}
- 
-   void push1(int x);
-   void push2(int x);
-   int pop1();
-   int pop2();
+    public:
+    
+    twoStacks(int n=100)
+    {
+        size = n; 
+        arr = new int[n]; 
+        top1 = -1; 
+        top2 = size;
+    }
+    
+    //Function to push an integer into the stack1.
+    void push1(int x)
+    {
+        top1++;
+        arr[top1] = x;
+        return;
+    }
+    
+    //Function to push an integer into the stack2.
+    void push2(int x)
+    {
+        top2--;
+        arr[top2] = x;
+        return;
+    }
+    
+    //Function to remove an element from top of the stack1.
+    int pop1()
+    {
+        if (top1 == -1)
+        return -1;
+        return arr[top1--];
+    }
+    
+    //Function to remove an element from top of the stack2.
+    int pop2()
+    {
+       if (top2 == size)
+       return -1;
+       return arr[top2++];
+    }
 };
 
 
+
+//{ Driver Code Starts.
 
 int main()
 {
@@ -55,43 +95,5 @@ int main()
         cout<<endl;
     }
 }
+
 // } Driver Code Ends
-
-
-
-
-
-//Function to push an integer into the stack1.
-void twoStacks :: push1(int x)
-{
-    if (top1 == size/2 - 1)
-    return;
-    arr[++top1] = x;
-    return;
-}
-   
-//Function to push an integer into the stack2.
-void twoStacks ::push2(int x)
-{
-    if (top2 == size/2)
-    return;
-    arr[--top2] = x;
-    return;
-}
-   
-//Function to remove an element from top of the stack1.
-int twoStacks ::pop1()
-{
-    if (top1 == -1)
-    return -1;
-    return arr[top1--];
-}
-
-//Function to remove an element from top of the stack2.
-int twoStacks :: pop2()
-{
-    if (top2 == size)
-    return -1;
-    return arr[top2++];
-}
-
