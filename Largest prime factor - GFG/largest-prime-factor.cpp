b4f2d3ce09f254f37934fb5495ff1708aa90efd4
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2023-08-09 10:55:24 +0530

    Added solution - LeetHub

--- Largest prime factor - GFG/largest-prime-factor.cpp
+++ Largest prime factor - GFG/largest-prime-factor.cpp
@@ -7,41 +7,16 @@ class Solution{
 public: 
     long long int largestPrimeFactor(int N){
         // code here
-        int size = sqrt(N);
-        int mx = 1;
-        for (int i = 1; i <= size; i++)
+        int num = 2;
+        while (num * num <= N)
         {
-            if ((N % i) == 0)
-            {
-                int j = N/i;
-                bool flag = true;
-                int sq = sqrt(j);
-                for (int k = 2; k <= sq; k++)
-                {
-                    if (j % k == 0)
-                    {
-                        flag = false;
-                        break;
-                    }
-                }
-                if (flag)
-                return j;
-                j = i;
-                flag = true;
-                sq = sqrt(j);
-                for (int k = 2; k <= sq; k++)
-                {
-                    if (j % k == 0)
-                    {
-                        flag = false;
-                        break;
-                    }
-                }
-                if (flag)
-                mx = i;
-            }
+            while (N % num == 0)
+            N /= num;
+            num++;
         }
-        return mx;
+        if (N == 1)
+        return num - 1;
+        return N;
     }
 };
 
