cb515cb997f27d729b3bff99c292a9b626fe643e
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2023-08-10 08:13:45 +0530

    Added solution - LeetHub

--- Longest Common Subsequence - GFG/longest-common-subsequence.cpp
+++ Longest Common Subsequence - GFG/longest-common-subsequence.cpp
@@ -10,23 +10,21 @@ class Solution
 {
     public:
     //Function to find the length of longest common subsequence in two strings.
-    int longest(string &s1, string &s2, vector<vector<int>> &dp, int i, int j)
-    {
-        int n = s1.size(), m = s2.size();
-        if (i == n || j == m)
-        return 0;
-        if (dp[i][j] != -1)
-        return dp[i][j];
-        if (s1[i] == s2[j])
-        return dp[i][j] = 1 + longest(s1, s2, dp, i + 1, j + 1);
-        return dp[i][j] = max(longest(s1, s2, dp, i + 1, j), longest(s1, s2, dp, i, j + 1));
-    }
     int lcs(int n, int m, string s1, string s2)
     {
         // your code here
-        vector<vector<int>> dp(n, vector<int> (m, -1));
-        for (int i = 0; i < )
-        return longest(s1, s2, dp, 0, 0);
+        vector<vector<int>> dp(2, vector<int> (m + 1, 0));
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int j = m - 1; j >= 0; j--)
+            {
+                if (s1[i] == s2[j])
+                dp[i][j] = 1 + dp[i + 1][j + 1];
+                else
+                dp[i][j] = max(dp[i + 1][j], dp[i][j + 1]);
+            }
+        }
+        return dp[0][0];
     }
 };
 
