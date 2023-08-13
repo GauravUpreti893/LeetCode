2d37116081e7bd632ff18d89cc9e8e6694245412
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2023-08-13 06:28:37 +0530

    Added solution - LeetHub

--- Nth Fibonacci Number - GFG/nth-fibonacci-number.cpp
+++ Nth Fibonacci Number - GFG/nth-fibonacci-number.cpp
@@ -7,20 +7,15 @@ using namespace std;
 // User function Template for C++
 class Solution {
   public:
-    int fibo(int n, vector<int> &dp)
-    {
-        if (n <= 2)
-        return 1;
-        if (dp[n] != -1)
-        return dp[n];
-        int mod = 1e9 + 7;
-        dp[n] = (fibo(n - 1, dp) + fibo(n - 2, dp)) % mod;
-    }
     int nthFibonacci(int n){
         // code here
-        vector<int> dp(n + 1, -1);
-        for (int i = 2)
-        return fibo(n, dp);
+        vector<int> dp(n + 1, 1);
+        int mod = 1e9 + 7;
+        for (int i = 3; i <= n; i++)
+        {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % mod;
+        }
+        return dp[n];
     }
 };
 
