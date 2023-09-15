c01adccb968394d3f0802cefddf36702099f923b
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2023-09-15 19:41:54 +0530

    Added solution - LeetHub

--- Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
+++ Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
@@ -9,36 +9,28 @@ using namespace std;
 
 class Solution{
 public:
-    int equalPartition(int N, int nums[])
+    bool check(int arr[], vector<vector<bool>> &dp, int idx, int sum)
+    {
+        int n = dp.size();
+        if (!sum)
+        return true;
+        if (idx == n || sum < 0)
+        return false;
+        if (!dp[idx][sum])
+        return false;
+        return dp[idx][sum] = check(arr, dp, idx + 1, sum) || check(arr, dp, idx + 1, sum - arr[idx]);
+    }
+    int equalPartition(int N, int arr[])
     {
         // code here
-        int n = N;
         int sum = 0;
-        for (int i = 0; i < n; i++)
-        {
-            sum += nums[i];
-        }
+        for (int i = 0; i < N; i++)
+        sum += arr[i];
         if (sum % 2)
-        return false;
+        return 0;
         sum /= 2;
-        if (nums[0] > sum)
-        return false;
-        vector<vector<bool>> dp(2, vector<bool> (sum + 1));
-        for (int i = 0; i < 2; i++)
-        {
-            dp[i][0] = 1;
-        }
-        dp[0][nums[0]] = 1;
-        for (int i = 1; i < n; i++)
-        {
-            for (int j = 1; j <= sum; j++)
-            {
-                dp[i%2][j] = dp[!(i%2)][j];
-                if (j - nums[i] >= 0)
-                dp[i%2][j] = dp[i%2][j] || dp[!(i%2)][j - nums[i]];
-            }
-        }
-        return dp[(n - 1)%2][sum];
+        vector<vector<bool>> dp(N, vector<bool> (sum + 1, 1));
+        return check(arr, dp, 0, sum);
     }
 };
 
