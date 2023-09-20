efc86ac7f92417f4e310d6be225e0904ae9f3609
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2023-09-20 16:11:41 +0530

    Added solution - LeetHub

--- Rotate Bits - GFG/rotate-bits.cpp
+++ Rotate Bits - GFG/rotate-bits.cpp
@@ -10,25 +10,17 @@ class Solution
         {
             //code here.
             d %= 16;
-            int ans1 = n, temp = 0, ans2 = n;
-            for (int i = 15; i > 15 - d; i--)
-            {
-                if (ans1 & (1<<i))
-                {
-                    ans1 ^= (1<<i);
-                    temp |= (1<<(i - 15 + d  - 1));
-                }
-            }
+            int ans1 = n, ans2 = n, temp;
+            temp = ~((1<<(15 - d + 1)) - 1);
+            temp &= n;
+            temp >>= (15 - d + 1);
+            ans1 &= ((1<<(15 - d + 1)) - 1);
             ans1 <<= d;
             ans1 |= temp;
-            temp = 0;
-            for (int i = 0; i < d; i++)
-            {
-                if (ans2 & (1<<i))
-                {
-                    temp |= (1<<(15 - d + i + 1));
-                }
-            }
+            
+            temp = ((1<<d) - 1);
+            temp &= n;
+            temp <<= (15 - d + 1);
             ans2 >>= d;
             ans2 |= temp;
             return {ans1, ans2};
